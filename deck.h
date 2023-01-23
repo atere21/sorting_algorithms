a91ffa0b84fa002efcb28395edd991d253871e7e
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2023-01-23 04:01:33 -0600

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,99 +1,50 @@
 #ifndef DECK_H
-
 #define DECK_H
 
-
-
-#include <stdlib.h>
-
-
-
 /**
-
- * enum kind_e - Enumeration of card suits.
-
- * @SPADE: Spades suit.
-
- * @HEART: Hearts suit.
-
- * @CLUB: Clubs suit.
-
- * @DIAMOND: Diamonds suit.
-
+ * enum kind_e - struct for card types
+ *
+ * @SPADE: Spade Cards
+ * @HEART: Heart Cards
+ * @CLUB: Club Cards
+ * @DIAMOND: Diamond Cards
  */
-
 typedef enum kind_e
-
 {
-
 	SPADE = 0,
-
 	HEART,
-
 	CLUB,
-
 	DIAMOND
-
 } kind_t;
 
-
-
 /**
-
  * struct card_s - Playing card
-
  *
-
  * @value: Value of the card
-
  * From "Ace" to "King"
-
  * @kind: Kind of the card
-
  */
-
 typedef struct card_s
-
 {
-
 	const char *value;
-
 	const kind_t kind;
-
 } card_t;
 
-
-
 /**
-
  * struct deck_node_s - Deck of card
-
  *
-
  * @card: Pointer to the card of the node
-
  * @prev: Pointer to the previous node of the list
-
  * @next: Pointer to the next node of the list
-
  */
-
 typedef struct deck_node_s
-
 {
-
 	const card_t *card;
-
 	struct deck_node_s *prev;
-
 	struct deck_node_s *next;
-
 } deck_node_t;
 
 
-
 void sort_deck(deck_node_t **deck);
-
-
 
 #endif /* DECK_H */
